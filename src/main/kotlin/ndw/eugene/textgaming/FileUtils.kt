f9341d6fba7658fe6593082934f366f36f2838b0
kuznetsov <Eugene.NDW@yandex.ru>
2023-01-06 15:25:55 +0200

    remove useless utils

--- src/main/kotlin/ndw/eugene/textgaming/FileUtils.kt
+++ src/main/kotlin/ndw/eugene/textgaming/FileUtils.kt
@@ -1,10 +1,5 @@
 package ndw.eugene.textgaming
 
-import java.net.URI
-import java.net.URL
-import java.nio.file.*
-import java.util.stream.Stream
-
 fun readBytesFromFile(illustration: String): ByteArray {
     val illustrationStream =
         TextGameApplication::class.java.getResourceAsStream("/illustrations/$illustration")
@@ -12,31 +7,5 @@ fun readBytesFromFile(illustration: String): ByteArray {
 
     return illustrationStream.use {
         it.readAllBytes()
-    }
-}
-
-fun walk(path: String, fileURL: URL): Stream<Path>? {
-    val uri = fileURL.toURI()
-
-    val paths = if ("jar" == uri.scheme) {
-        safeWalkJar(path, uri)
-    } else {
-        Files.walk(Paths.get(uri))
-    }
-
-    return paths
-}
-
-private fun safeWalkJar(path: String, uri: URI): Stream<Path>? {
-    val fs = getFileSystem(uri)
-    val path1 = fs.getPath(path)
-    return Files.walk(path1)
-}
-
-private fun getFileSystem(uri: URI): FileSystem{
-    return try {
-        FileSystems.getFileSystem(uri)
-    } catch (e: FileSystemNotFoundException) {
-        FileSystems.newFileSystem(uri, emptyMap<String, String>())
     }
 }
